908342a2f5e8a85d495614a3942594608916d880
umesh594 <bt21ece117@iiitn.ac.in>
2026-03-27 21:06:21 +0530

    codes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,11 @@ const dashboardRoutes = require('./routes/dashboard');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
